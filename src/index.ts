8ac0da2483e11b0f83014460bd1ed46307e44b75
Alexandre Switalski <alexandre.swit@gmail.com>
2023-02-03 22:55:46 +0100

    feat: cli

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,59 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+import { exit } from 'process'
+import { data } from './data/mock'
+
+const ALLOWED_FEATURES = ['--filter', '--count']
+
+const checkArgumentsFromCli = (args: string[]) => {
+  const givenArgument = args.slice(2)[0]
+  if (!givenArgument) {
+    throw new Error('No feature given')
+  }
+
+  const isAnExistingFeature = ALLOWED_FEATURES.find((feature: string) => givenArgument.startsWith(feature))
+  if (!isAnExistingFeature) {
+    throw new Error('Available features: --filter=<your search> and --count')
+  }
+  return givenArgument
+}
+
+const filterMode = (filterValueFromCli: string) => {
+  const filterValue = filterValueFromCli.slice('--filter='.length)
+  if (!filterValue) {
+    throw new Error('Usage: --filter=<your criteria>')
+  }
+
+  
+  return getCountrieWithAnimalsFromCritera(filterValue)
+}
+
+const getCountrieWithAnimalsFromCritera = (filterValue: string): Country[] => {
+  
+
+  return data.map((country) => ({
+    name: country.name,
+    people: _checkAnimalsForCriteria(country.people),
+  }))
+
+}
+
+
+const countMode = () => {
+  //
+}
 
 const start = () => {
-  console.log('start')
-  console.log(process.env.EXAMPLE_VALUE)
+  const app = checkArgumentsFromCli(process.argv)
+  
+  if (app.startsWith('--filter')) {
+    filterMode(app)
+    return
+  }
+
+  return countMode()
 }
 
 start()
+exit()
