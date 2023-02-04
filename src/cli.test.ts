6f7a4cdddf753755adcfec39e510b8dc3c382d05
Alexandre Switalski <alexandre.swit@gmail.com>
2023-02-04 17:59:52 +0100

    ci: cli test unit

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,12 +1,22 @@
-import { assert, describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { checkArgumentsFromCli } from './cli'
 
 describe('CLI suite tests', () => {
   it('Should return string containing filter or count', () => {
-    //
+    const cliArgs = ['yarn', 'start', '--filter=ly']
+    expect(checkArgumentsFromCli(cliArgs, ['--filter'])).toBe(cliArgs[2])
   })
 	
   it('Should throw an error if given arguments are not compliant', () => {
     //
+    const cliArgs = ['yarn', 'start']
     const invalidArgs = ['--filter ly', 'filter', 'filter=ff', 'count', 'a', '--a']
+    invalidArgs.forEach((criteria) => {
+      try {
+        checkArgumentsFromCli([...cliArgs, criteria], [])
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error)
+      }
+    })
   })
 })
